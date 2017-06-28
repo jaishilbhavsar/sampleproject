17c8788f683e20b72a0caf29dce6553da3169cc1
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2017-06-28 16:25:06 +0530

    new database with all tables

--- project_db.sql
+++ project_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2017 at 10:36 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: Jun 28, 2017 at 12:53 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `project_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill_tbl`
+--
+
+CREATE TABLE `bill_tbl` (
+  `bill_no` int(11) NOT NULL,
+  `fk_email_id` varchar(50) NOT NULL,
+  `fk_prod_id` int(11) NOT NULL,
+  `bill_amount` double NOT NULL,
+  `bill_date` date NOT NULL,
+  `ship_address` varchar(200) NOT NULL,
+  `pay_type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,6 +63,27 @@ CREATE TABLE `cat_tbl` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product_tbl`
+--
+
+CREATE TABLE `product_tbl` (
+  `prod_id` int(11) NOT NULL,
+  `prod_name` varchar(20) NOT NULL,
+  `prod_price` double NOT NULL,
+  `prod_mfg` varchar(50) NOT NULL,
+  `prod_color` varchar(10) NOT NULL,
+  `prod_img1` varchar(100) NOT NULL,
+  `prod_img2` varchar(100) NOT NULL,
+  `prod_img3` varchar(100) NOT NULL,
+  `prod_warranty` varchar(10) NOT NULL,
+  `prod_soh` varchar(10) NOT NULL,
+  `prod_desc` varchar(50) NOT NULL,
+  `fk_cat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_tbl`
 --
 
@@ -68,6 +105,12 @@ CREATE TABLE `user_tbl` (
 --
 
 --
+-- Indexes for table `bill_tbl`
+--
+ALTER TABLE `bill_tbl`
+  ADD PRIMARY KEY (`bill_no`);
+
+--
 -- Indexes for table `cart_tbl`
 --
 ALTER TABLE `cart_tbl`
@@ -80,6 +123,12 @@ ALTER TABLE `cat_tbl`
   ADD PRIMARY KEY (`pk_cat_id`);
 
 --
+-- Indexes for table `product_tbl`
+--
+ALTER TABLE `product_tbl`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
 -- Indexes for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
@@ -90,6 +139,11 @@ ALTER TABLE `user_tbl`
 --
 
 --
+-- AUTO_INCREMENT for table `bill_tbl`
+--
+ALTER TABLE `bill_tbl`
+  MODIFY `bill_no` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `cart_tbl`
 --
 ALTER TABLE `cart_tbl`
@@ -99,6 +153,11 @@ ALTER TABLE `cart_tbl`
 --
 ALTER TABLE `cat_tbl`
   MODIFY `pk_cat_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_tbl`
+--
+ALTER TABLE `product_tbl`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
